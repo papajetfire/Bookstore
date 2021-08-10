b87c74fba3da41cf62ad4b91913596491a3e181f
Soumya Sheel <soumyasheel2011@gmail.com>
2021-08-10 10:00:05 +0530

    Fist Component

---
+++ src/firstComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+function firstComponent(){
+    return(
+        <React.Fragment>
+            <div>
+                <h1>
+                    First React Component
+                </h1>
+            </div>
+        </React.Fragment>
+    )
+}
+export default firstComponent
