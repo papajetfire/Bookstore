0eaa0269b73d6970fafe2ce5944984af0ffc6478
Soumya Sheel <soumyasheel2011@gmail.com>
2021-08-10 10:51:16 +0530

    Multiple components added

---
+++ src/myDeltails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MyDeltails() {
+    return (
+        <div>
+            <h1> Soumya Sheel</h1>
+            <h3> 22 years</h3>
+            <h3> 6'1</h3>
+            <h3> 185 lbs </h3>
+            <img src='88557285-cc73-4140-8e4c-f4b28d0ba22f.jpg' height="200" width="120"></img>
+            
+        </div>
+    )
+}
+
+export default MyDeltails
